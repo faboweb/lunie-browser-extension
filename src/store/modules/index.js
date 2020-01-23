2b028417e1b6cae67e6c34fc5969997090b2d8b4
iambeone <iambeone@gmail.com>
2020-01-23 18:06:29 +0500

    session added

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
 export default opts => ({
-  connection: require(`./connection.js`).default(opts)
+  connection: require(`./connection.js`).default(opts),
+  session: require(`./session.js`).default(opts)
 })
