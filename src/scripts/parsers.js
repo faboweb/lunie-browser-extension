c16299db6be7ba696d61a081dc16fcd0d175990f
Bitcoinera <anagjordano@gmail.com>
2020-04-18 00:44:39 +0200

    go the easy route. start displaying polkadot tx

--- src/scripts/parsers.js
+++ src/scripts/parsers.js
@@ -1,14 +1,12 @@
 'use strict'
 
 import { parseCosmosTx } from './cosmosTxParser'
-import { parsePolkadotTx } from './polkadotTxParser'
 
 export const parseTx = (signRequest, network, lunieTransaction) => {
   console.log('signRequest', signRequest) // for now because of linter
-  if (network.network_type === 'cosmos') {
-    return parseCosmosTx(network, lunieTransaction)
-  }
-  if (network.network_type === 'polkadot') {
-    return parsePolkadotTx(network, lunieTransaction)
-  }
+  return parseCosmosTx(network, lunieTransaction)
+
+  // if (network.network_type === 'polkadot') {
+  //   return parsePolkadotTx(network, lunieTransaction)
+  // }
 }
